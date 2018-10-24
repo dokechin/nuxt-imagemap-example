0379f38bf56101bdee07db708d438fe89256c2b9
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-10-25 01:32:55 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="container">
+     <nuxt-link class="btn btn-primary btn-block badge-pill btn-lg" to="/test/" role="button">スタート</nuxt-link>
+    </section>
+</template>
+
+<script>
+</script>

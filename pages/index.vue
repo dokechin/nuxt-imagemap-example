a4efa011f0e346605799e051e72fc8dfe629710a
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-10-25 01:40:33 +0900

    add links

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="container">
-     <nuxt-link class="btn btn-primary btn-block badge-pill btn-lg" to="/test/" role="button">スタート</nuxt-link>
+     <nuxt-link class="btn btn-primary btn-block badge-pill btn-lg" to="/test/" role="button">Start(NG)</nuxt-link>
+     <nuxt-link class="btn btn-primary btn-block badge-pill btn-lg" to="/index2" role="button">Start(NG)</nuxt-link>
     </section>
 </template>
 
